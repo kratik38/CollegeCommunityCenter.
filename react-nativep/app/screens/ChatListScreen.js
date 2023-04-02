75279c2906157b2015e0eb8eb0d64b31d1053046
kratik <kratik38@gmail.com>
2023-04-02 10:50:16 +0530

    adding rightheader button in chatlistscreen

--- react-nativep/app/screens/ChatListScreen.js
+++ react-nativep/app/screens/ChatListScreen.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Button, View,Text,StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const ChatListScreen = props => {
+
+	useEffect(()=>{
+			props.navigation.setOptions({
+				headerRight:()=>{
+						return <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+						<Item
+						title="New chat"
+						iconName='create-outline'
+						onPress={()=>{
+
+						}}/>
+					</HeaderButtons>
+				}
+			})
+	},[]);
 
 	return <View style={styles.container}>
 		<Text>This is the chat list screen.</Text>
 		<Button title="go to Chat Screen" onPress={()=>{props.navigation.navigate("ChatScreen")}}/>
+		<Button title="go to Chat settings" onPress={()=>{props.navigation.navigate("ChatSettings")}}/>
 	</View>
 }
 
