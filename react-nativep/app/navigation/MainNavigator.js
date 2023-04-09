4111e37beb9cc218c3bd805ef0e01e27a88cca9a
kratik <kratik38@gmail.com>
2023-04-09 11:21:34 +0530

    chat data
    when the app loads retriveing the user chatIds from firebase
    loop over them one by one and
    get the actual data from the chats and showing the data
    in ChatListScreen(right now console) as userChats.

--- react-nativep/app/navigation/MainNavigator.js
+++ react-nativep/app/navigation/MainNavigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
@@ -9,6 +9,10 @@ import SettingsScreen from '../screens/SettingsScreen';
 import ChatScreen from '../screens/ChatScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NewChatScreen from '../screens/NewChatScreen';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFirebaseApp } from '../utils/firebaseHelper';
+import { child, getDatabase, off, onValue, ref } from 'firebase/database';
+import { setChatsData } from '../store/chatSlice';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();  
@@ -36,9 +40,9 @@ const TabNavigator = ()=>{
   )
 }
 
-const MainNavigator = ()=>{
+const StackNavigator = ()=>{
 
-	return (
+  return (
 			<Stack.Navigator>
         <Stack.Group>
 				<Stack.Screen name="Home" component={TabNavigator} options={{ headerShown:false }}/>
@@ -60,6 +64,72 @@ const MainNavigator = ()=>{
          component={NewChatScreen}/>
         </Stack.Group>
 			</Stack.Navigator>
+  )
+}
+
+const MainNavigator = ()=>{
+  
+  const dispatch = useDispatch();
+  
+  const userData = useSelector(state=>state.auth.userData);
+  const storedUsers = useSelector(state=>state.users.storedUsers);
+
+  useEffect(()=>{
+
+    console.log("subscribing to firebase listeners");
+
+    const app = getFirebaseApp();
+    const dbRef = ref(getDatabase(app));
+    const userChatsRef = child(dbRef,`userChats/${userData.userId}`);
+
+    const refs = [userChatsRef];
+
+    onValue(userChatsRef,(querySnapshot)=>{
+      const chatIdsData = querySnapshot.val() || {};
+      const chatIds = Object.values(chatIdsData);
+
+      const chatsData = {};
+      let chatsFoundCount = 0;
+
+      for (let i = 0; i < chatIds.length; i++) {
+        const chatId = chatIds[i];
+        const chatRef = child(dbRef,`chats/${chatId}`);
+        refs.push(chatRef);
+
+        onValue(chatRef,(chatSnapshot)=>{
+
+          chatsFoundCount++;  
+
+          const data = chatSnapshot.val();
+
+          if(data){
+            data.key = chatSnapshot.key;
+
+            chatsData[chatSnapshot.key] = data;
+          }
+
+          if(chatsFoundCount >= chatIds.length){
+            dispatch(setChatsData({ chatsData }));
+          }
+          
+        })
+        
+      }
+
+      console.log(chatIds);
+       
+    })
+
+
+    return ()=>{
+      console.log("unsubscribing firebase listener");
+      refs.forEach(ref => off(ref));
+    }
+
+  },[])
+
+	return (
+    <StackNavigator/>
 	)
 }
 
